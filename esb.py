8c1cf31f3301e276d7812b785fa6d9dee1efc617
Stefanos <sagelastos@gmail.com>
2022-06-13 00:08:25 +0200

    started some work in transformer, json works

--- esb.py
+++ esb.py
@@ -1,59 +1,20 @@
 from bottle import get, post, run, response, request
 import json, auth, esb_transform as transform
-import redis
-from datetime import datetime
-
-
-r = redis.Redis(
-    host="localhost",
-    port=9000,
-    db=0,
-    password="eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81",
-    charset="utf-8",
-    decode_responses=True,
-)
-
-
-# r.hset("user:12345", mapping={"id": "1", "email": "@a", "token": "12345"})
-# r.hset("user:67890", mapping={"id": "2", "email": "@b", "token": "67890"})
 
 ############## THIS IS ABOUT READING MESSAGES
 @get("/topic/<topic>/limit/<limit:int>")
 def _(topic, limit):
     auth.verify_token()
-    response.content_type = "application/json"
+    content_type = validate_content_type()
+    response.content_type = content_type
     try:
         # Validation
         if limit <= 0:
             raise Exception(f"limit cannot be {limit}")
-
-        # Get keys related to partial search of 'Topic'
-        keys = r.keys("*" + topic + "*")
-        # Validation
-        if keys == None:
-            raise Exception(f"No keys found with {topic}")
-
-        timestamps = {}
-        messages = {}
-        n = 0
-        # Loop thrigh keys and timestamps of those keys
-        for key in keys:
-            message = r.hgetall(key)
-            messages[n] = message
-            timestamps[n] = datetime.fromtimestamp(int(key[-10:]))
-            n += 1
-
-        # Sort by timestamps & retrieve their keys
-        sorted_timestamps = list(
-            dict(sorted(timestamps.items(), key=lambda x: x[1])).keys()
-        )
-        # index by custom limit (latest timestamps)
-        index_out = sorted_timestamps[-limit:]
-
-        # Dict comprehension -> return messages matching indexed keys
-        return {k: messages[k] for k in index_out}
+        return transform.get_messages(topic, limit, content_type)
 
     except Exception as ex:
+        raise
         response.status = 400
         return str(ex)
 
@@ -65,6 +26,13 @@ def _(topic):
     token_data = auth.verify_token()
     provider_id = token_data["provider_id"]
     print(provider_id)
+    content_type = validate_content_type()
+    transform.save_message(
+        body=request.body, topic=topic, type=content_type, author=provider_id
+    )
+
+
+def validate_content_type():
     content_type = request.headers.get("Content-Type", None)
     if content_type not in transform.allowed_types:
         response.status = 415
@@ -72,9 +40,7 @@ def _(topic):
             "code": "invalid_content_type",
             "description": f"Received: {content_type}, we support: {transform.allowed_types}",
         }
-    transform.save_message(
-        body=request.body, topic=topic, type=content_type, author=provider_id
-    )
+    return content_type
 
 
 # message =
--- esb_transform.py
+++ esb_transform.py
@@ -1,6 +1,9 @@
 import xmltodict
+from dicttoxml import dicttoxml
 import redis, json, uuid, time, calendar, csv, io
 import yaml
+from datetime import datetime
+
 
 allowed_types = {
     "application/json",
@@ -12,8 +15,56 @@ allowed_types = {
 records_expiration = 120
 
 r = redis.Redis(
-    host="localhost", port=9000, db=0, password="eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81", charset="utf-8", decode_responses=True
+    host="localhost",
+    port=9000,
+    db=0,
+    password="eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81",
+    charset="utf-8",
+    decode_responses=True,
 )
+
+
+def get_messages(topic, limit, type):
+    # Get keys related to partial search of 'Topic'
+    keys = r.keys("*/topic:" + topic + "/uid:*")
+    # Validation
+    if keys == None:
+        raise Exception(f"No keys found with {topic}")
+    timestamps = {}
+    messages = {}
+    n = 0
+    # Loop thrigh keys and timestamps of those keys
+    for key in keys:
+        message = r.hgetall(key)
+        messages[n] = message
+        timestamps[n] = datetime.fromtimestamp(int(key[-10:]))
+        n += 1
+    # Sort by timestamps & retrieve their keys
+    sorted_timestamps = list(
+        dict(sorted(timestamps.items(), key=lambda x: x[1])).keys()
+    )
+    # index by custom limit (latest timestamps)
+    index_out = sorted_timestamps[-limit:]
+    # Dict comprehension -> return messages matching indexed keys
+    sorted_messages = {k: messages[k] for k in index_out}
+
+    # Transform the messages
+    if type == "application/json":
+        return sorted_messages
+    elif type == "application/xml":
+        return dicttoxml(sorted_messages,  attr_type=False)
+    elif type == "application/x-yaml":
+        body_dict = yaml.safe_load(sorted_messages)
+        print(body_dict)
+    # elif type == "text/tab-separated-values":
+    #     body_dict = ""
+    #     byte_str = body.read()
+    #     text_obj = byte_str.decode("UTF-8")
+    #     rd = csv.reader(io.StringIO(text_obj), delimiter="\t", quotechar='"')
+    #     print(rd[1])
+        # for row in rd:
+        #     print(row)
+    return
 
 
 def save_message(body, type, topic, author):
